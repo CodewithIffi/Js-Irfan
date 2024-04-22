c032caac6ddd55a9aac9e57d3675313fe81bff3a
Irfan khan <153745317+CodewithIffi@users.noreply.github.com>
2024-04-22 17:32:45 +0000

    Today i learned about Variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1122
+let accountEmail = "Irfan@yahoo.com"
+var accountPassword = "12345"
+accountCity = "karachi"
+
+console.log(accountId);
+// accountId = 2 // Not Allowed
+
+
+
+console.table([ accountId ,accountEmail , accountPassword ,accountCity ])
